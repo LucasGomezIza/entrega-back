6b7ea0c6f7e1ac17a3e57e93a84b69005728dfe0
Lucas Gomez Izarrualde <lgizarrualde@infraestructura.gob.ar>
2025-09-12 14:03:55 -0300

    controllers

---
+++ proyecto/src/models/product.model.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require('mongoose')
+const mongoosePaginate = require('mongoose-paginate-v2')
+
+const ProductSchema = new Schema({
+    title: { type: String, required: true, index: true },
+    description: { type: String, default: '' },
+    code: { type: String, required: true, unique: true, index: true },
+    price: { type: Number, required: true, index: true },
+    status: { type: Boolean, default: true },
+    stock: { type: Number, required: true },
+    category: { type: String, required: true, index: true },
+    thumbnails: { type: [String], default: [] }
+    }, { timestamps: true });
